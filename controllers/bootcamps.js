1a6b10e0f4530e07277be8e08a690cf696251b74
bilelkh <bilel1996k@gmail.com>
2019-10-26 23:57:08 +0100

    complete crud

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,21 +1,31 @@
-
-
+const Bootcamp = require('../models/Bootcamps');
 
 //@desc Get all bootcamps 
 //@route GET /api/v1/bootcamps 
 //@access Public 
 
-exports.getBootcamps =(req,res,next)=>{
-    res.status(200).json({success:true,msg:'Show all bootcamps'})
+exports.getBootcamps = async(req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+        res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps })
+    } catch (err) {
+       // res.status(400).json({ success: false })
+       next(error)
+    }
 }
-
 
 //@desc Get Single bootcamps 
 //@route GET /api/v1/bootcamps/:id 
 //@access Public 
 
-exports.getBootcamp =(req,res,next)=>{
-    res.status(200).json({success:true,msg:'Get Single bootcamp'})
+exports.getBootcamp = async(req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+        res.status(200).json({ success: true, data: bootcamp });
+    } catch (err) {
+        res.status(400).json({ succes: false })
+    }
+
 }
 
 
@@ -23,8 +33,21 @@ exports.getBootcamp =(req,res,next)=>{
 //@route POST /api/v1/bootcamps 
 //@access Public 
 
-exports.postBootcamp =(req,res,next)=>{
-    res.status(200).json({success:true,msg:'Creat new bootcamp'})
+exports.postBootcamp = async(req, res, next) => {
+    // console.log('===req.body===',req.body)
+    // res.status(200).json({success:true,msg:'Creat new bootcamp'})
+
+    try {
+        const bootcamp = await Bootcamp.create(req.body);
+        res.status(201).json({ success: true, data: bootcamp })
+
+        if (!bootcamp) { res.status(400).json({ success: false }) }
+
+
+
+    } catch (error) {
+        res.status(400).json({ succes: false })
+    }
 }
 
 
@@ -33,9 +56,22 @@ exports.postBootcamp =(req,res,next)=>{
 //@route GET /api/v1/bootcamps/:id
 //@access Private 
 
-exports.updateBootcamp =(req,res,next)=>{
-    console.log("===req.params===",req.params)
-    res.status(200).json({success:true,msg:`Update bootcamp ${req.params.id}`})
+exports.updateBootcamp = async(req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+        if (!bootcamp) {
+            return res.status(400).json({ success: false })
+        }
+
+        res.status(200).json({ succes: true, data: bootcamp })
+    } catch (error) {
+        return res.status(400).json({ success: false })
+
+    }
+
 }
 
 
@@ -43,6 +79,18 @@ exports.updateBootcamp =(req,res,next)=>{
 //@route GET /api/v1/bootcamp
 //@access Private 
 
-exports.deleteBootcamp =(req,res,next)=>{
-    res.status(200).json({success:true,msg:`Delete bootcamp ${req.params.id}`})
+exports.deleteBootcamp = async(req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+        if (!bootcamp) {
+            return res.status(400).json({ succes: false });
+        }
+
+        res.status(200).json({ success: true, data: {} });
+
+    } catch (err) {
+        res.status(400).json({ success: false })
+    }
+
 }
--- server.js
+++ server.js
@@ -15,6 +15,8 @@ connectDB();
 //Load env vars 
 
 const app = express();
+app.use(express.json()) ; 
+
 
 const logger = (req, res, next) => {
         req.hello = 'Hello word';
@@ -32,7 +34,7 @@ app.get('/', (req, res) => {
     res.send("Hello from express")
 })
 const PORT = process.env.PORT;
-console.log("===MONGO_URI===",process.env.PORT)
+console.log("===MONGO_URI===", process.env.PORT)
 
 app.listen(PORT, console.log(`SERVER RUNNING IN ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
 
@@ -41,5 +43,4 @@ process.on('unhandledRejection', (err, promise) => {
     console.log(`Error: ${err.message}`.red);
     // Close server & exit process
     // server.close(() => process.exit(1));
-  });
-  
+});
